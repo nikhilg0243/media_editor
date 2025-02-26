495d2bd7d3ee3ce450a0fe397453dbd3a6a89b9a
ecarry <lianshiliang93@gmail.com>
2025-02-26 16:16:35 +0800

    migrations dir change dir

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,7 @@ config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "./src/db/schema",
+  out: "./src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
